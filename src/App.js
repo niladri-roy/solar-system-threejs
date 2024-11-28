e43a2b04b0d487baedb1e8a23b511236a9a43c91
Niladri Roy <royniladri2017@gmail.com>
2024-11-28 22:13:07 +0530

    "Added CSS reset and basic styling, introduced React Three Fiber for 3D rendering, and replaced App component with a SolarSystem component containing a Sun mesh."

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 
+import { Canvas } from '@react-three/fiber';
+
+const Sun = () => {
+  return (
+    <mesh>
+      <sphereGeometry args={[2, 32, 32]} />
+      <meshBasicMaterial color="yellow" />
+      <pointLight intensity={2} />
+    </mesh>
+  );
+};
+
+const SolarSystem = () => {
+  return (
+    <Canvas
+      camera={{ position: [0, 10, 20], fov: 75 }}
+    >
+
+      <Sun />
+    </Canvas>
+  );
+};
+
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <SolarSystem />
   );
 }
 
